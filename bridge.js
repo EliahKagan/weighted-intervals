8b9b98ece1968afdf804b333551eb0e7ee7fb3e5
Eliah Kagan <degeneracypressure@gmail.com>
2023-04-01 18:18:42 -0400

    Bump Pyodide from 0.22.1 to 0.23.0
    This uses Python 3.11 (rather than 3.10 as previously).
    
    The specific Python version that Pyodide 0.23.0 uses is 3.11.2.
    
    Since this is a move from Python 3.10 to Python 3.11 for this
    project, I'm going ahead and bumping the project version from alpha
    4+ to alpha 5 as well.

--- bridge.js
+++ bridge.js
@@ -127,7 +127,7 @@
 
         await tryRun(true, _e => 'Oh no, Pyodide failed to load!',
             async () => py = await loadPyodide({
-                indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.22.1/full/'
+                indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.23.0/full/'
             }));
 
         status.innerText =
